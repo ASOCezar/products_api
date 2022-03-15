7f1fd18a1f3048df88e73d199a03056129e0a201
Cezar Oliveira <aso.cezar00@gmail.com>
2022-03-15 14:59:07 -0300

    Show product method

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,8 +5,22 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 
 class ProductController extends Controller {
+
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
     public function index() {
-        return Product::all();
+        $data = ['data' => $this->product->all()];
+        return response()->json($data);
+    }
+
+    public function show(Product $id) {
+        $data = ['data' => $id];
+        return response()->json($data);
     }
 }
 
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::prefix('/products')->group(function() {
+    Route::get('/', [ProductController::class, 'index']);
+
+    Route::get('/{id}', [ProductController::class, 'show']);
+});
